e25ed79e224b6e1b3e1bf0eb46253d23de2d8bde
Gift Amah <codedgift@gmail.com>
2023-03-31 15:08:04 +0100

    Formated response structure

---
+++ app/Http/Controllers/AppBaseController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ResponseCodeTrait;
+use App\Utilities\GeneralConstants;
+
+class AppBaseController extends Controller
+{
+    use ResponseCodeTrait;
+    //
+
+    /**
+     * @param $data
+     * @param $resp_message
+     * @param $resp_description
+     * @param string $resp_code
+     * @return mixed
+     */
+    public function successResponse($data, $resp_message, $resp_description, string $resp_code = "00")
+    {
+        return \Response::json([
+            "resp_code" => $resp_code,
+            "resp_message" => $resp_message,
+            "resp_description" => $resp_description,
+            "data" => $data,
+            "errors" => null
+        ], self::$STATUS_CODE_DONE);
+    }
+
+    /**
+     * @param $error
+     * @param $resp_message
+     * @param $resp_description
+     * @param string $resp_code
+     * @return mixed
+     */
+    public function errorResponse($error, $resp_message, $resp_description, string $resp_code = "01")
+    {
+        return \Response::json([
+            "resp_code" => $resp_code,
+            "resp_message" => $resp_message ?? $error,
+            "resp_description" => $resp_description ?? $error,
+            "data" => null,
+            "errors" => $error
+        ], self::$STATUS_CODE_NOT_VALID);
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function generalError()
+    {
+        return (new self())->errorResponse(
+            "An error occurred",
+            GeneralConstants::ERROR_TEXT,
+            GeneralConstants::ERROR_CODE,
+            "An error occurred"
+        );
+    }
+}
