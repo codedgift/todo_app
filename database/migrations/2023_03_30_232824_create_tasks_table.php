60fdac313b78323619a5b4984597cbcbbd0f843e
Gift Amah <codedgift@gmail.com>
2023-03-31 01:02:11 +0100

    Update task migration table and model

--- database/migrations/2023_03_30_232824_create_tasks_table.php
+++ database/migrations/2023_03_30_232824_create_tasks_table.php
@@ -14,8 +14,22 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('user_id');
+            $table->bigInteger('category_id');
+            $table->bigInteger('visitor_id')->nullable();
+            $table->enum('status', ['0', '1', '2'])->default('0')->comment('0 means Not Started, 1 means Completed, 2 menas In Progress');
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
+            $table->softDeletes();
+
+            // Foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
