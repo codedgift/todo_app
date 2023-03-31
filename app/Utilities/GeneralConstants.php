d3c79a3049f2f98d640b341fb1e559f32470f33d
Gift Amah <codedgift@gmail.com>
2023-03-31 15:08:28 +0100

    Added general api constraints

---
+++ app/Utilities/GeneralConstants.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Utilities;
+
+
+class GeneralConstants
+{
+
+    const SUCCESS_TEXT = "success";
+    const PENDING_TEXT = "pending";
+    const ERROR_TEXT = "error";
+
+    const SUCCESS_CODE = "00";
+    const ERROR_CODE = "01";
+    const PENDING_CODE = "02";
+}
