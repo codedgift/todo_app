e38f568f2012ed9c8e7a41cf76a6b228ba7148b8
Gift Amah <codedgift@gmail.com>
2023-04-01 05:05:45 +0100

    Send welcome mail

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ActivationMail;
+use App\Mail\WelcomeMail;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -16,6 +17,16 @@ class SendMailController extends Controller
     {
         try{
             Mail::to($user['email'])->queue(new ActivationMail($user, $confirmation_code));
+        } catch (Exception $e) {
+            Log::info($e->getMessage());
+        }
+
+    }
+
+    public static function sendWelcomeMail($user)
+    {
+        try{
+            Mail::to($user['email'])->queue(new WelcomeMail($user));
         } catch (Exception $e) {
             Log::info($e->getMessage());
         }
---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(config('mail.from.address'), config('mail.from.name'))
+                ->subject('Welcome On Board')
+                ->view('mail.welcomeemail', ['data' => $this->user, 'subject'=>'Welcome On Board']);
+    }
+}
